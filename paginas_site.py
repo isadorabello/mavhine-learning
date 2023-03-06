dcc07ea92e520f1a0bb5358c85e35957cca15306
isadorabello <isadora.bello@gec.inatel.br>
2023-03-06 20:50:11 -0300

    making progress

--- paginas_site.py
+++ paginas_site.py
@@ -1,4 +1,6 @@
 import pandas as pd
+from sklearn.svm import LinearSVC
+from sklearn.metrics import accuracy_score
 
 uri = "https://gist.githubusercontent.com/guilhermesilveira/2d2efa37d66b6c84a722ea627a897ced/raw/10968b997d885cbded1c92938c7a9912ba41c615/tracking.csv"
 
@@ -19,3 +21,19 @@ y = dados["comprou"]
 
 # print(x.head())
 # print(y.head())
+
+treino_x = x[:75]
+treino_y = y[:75]
+teste_x = x[75:]
+teste_y = y[75:]
+
+print("Treino: %d e Teste: %d" % (len(treino_x), (len(teste_x))))
+
+model = LinearSVC()
+model.fit(treino_x,treino_y)
+
+previsoes = model.predict(teste_x)
+acertos = (previsoes==teste_y).sum()
+total = len(teste_x)
+
+print("acuraria: %.2f%%" % (accuracy_score(teste_y, previsoes) *100))
